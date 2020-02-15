c670dd4cfa598e22c4c7a4af77a388b3e15dd0af
kevin <948876386@qq.com>
2020-02-15 15:46:23 +0800

    init

---
+++ delete-daemonset.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+#tail -n +2，表示从第2行到最后一行
+pods=`kubectl get DaemonSet -n rook-ceph | awk '{print $1}' | tail -n +2`
+
+echo $pods
+
+for i in $pods
+do
+  kubectl -n rook-ceph delete DaemonSet $i --force --grace-period=0
+done
+#--force --grace-period=0，表示强制性
