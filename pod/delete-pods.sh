e7cd4212a3513ed3b14dc4fc925335df2df2a5cc
kevin <948876386@qq.com>
2020-02-15 15:47:52 +0800

    init

--- pod/delete-pods.sh
+++ pod/delete-pods.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+#tail -n +2, 表示从第2行到最后一行
 pods=`kubectl get pods -n rook-ceph | awk '{print $1}' | tail -n +2`
 
 echo $pods
@@ -8,3 +9,4 @@ for i in $pods
 do
   kubectl -n rook-ceph delete pods $i --force --grace-period=0
 done
+#--force --grace-period=0, 表示强制性
